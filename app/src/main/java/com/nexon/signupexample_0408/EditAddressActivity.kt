6e38a6f5c3a9b02c8ec24cb15e9fb92da87a2a6d
RobertHan96 <yshan4329@gmail.com>
2020-04-08 22:21:17 +0900

    11th commit : 데이터를 다른 화면에서 가지고 돌아오는 코드 추가

--- app/src/main/java/com/nexon/signupexample_0408/EditAddressActivity.kt
+++ app/src/main/java/com/nexon/signupexample_0408/EditAddressActivity.kt
@@ -1,12 +1,31 @@
 package com.nexon.signupexample_0408
 
+import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_edit_address.*
 
 class EditAddressActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_edit_address)
+
+        okBtn.setOnClickListener {
+//       주소 입력을 완료함을 의미함, 이전 화면으로 돌아가면서 주소 데이터를 반환해야 함
+//       데이터 반환용 인텐트는 출발지, 도착지를 함수에 적지않아도 됨
+            val resultIntent = Intent()
+            resultIntent.putExtra("mainAddress", mainAddressEdit.text.toString())
+            resultIntent.putExtra("detailAddress", detailAddressEdit.text.toString())
+            
+//          위에서 가공한 데이터를 이전 화면으로 돌리기 위한 setResult함수 (입력완료를 의미하는 RESULT_OK 호출)
+            setResult(Activity.RESULT_OK, resultIntent)
+//          finish를 통해 이전화면으로 이동함
+            finish()
+
+        }
+        
+        
     }
 }
--- app/src/main/java/com/nexon/signupexample_0408/EditNameActivity.kt
+++ app/src/main/java/com/nexon/signupexample_0408/EditNameActivity.kt
@@ -1,11 +1,15 @@
 package com.nexon.signupexample_0408
 
+import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_edit_name.*
 
 class EditNameActivity : AppCompatActivity() {
+//   요청코드는 원래 숫자로 적지만, 숫자만 적으면 나중에 구분이 어렵기때문에 보통 이유를 멤버 변수로 만들어서
+//   startActivityForResult 함수에 사용함, 코드에 넣을 숫자는 고유해야 함
+    val REQ_FOR_ADDRESS = 1000
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,6 +22,25 @@ class EditNameActivity : AppCompatActivity() {
             newIntent.putExtra("userName", inputName)
             newIntent.putExtra("userAge", 2020-inputBirthYear+1)
             startActivity(newIntent)
+        }
+
+        inputAddressBtn.setOnClickListener {
+            val myIntent2 = Intent(this,EditAddressActivity::class.java)
+            startActivityForResult(myIntent2, REQ_FOR_ADDRESS)
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+//      인텐트 요청 코드가 내가 셋팅한 것과 일치하는지 확인
+        if ( requestCode == REQ_FOR_ADDRESS) {
+//       해당 코드의 결과값도 정상(RESULT_OK)라면
+            if (resultCode == Activity.RESULT_OK) {
+                val mainAddress = data?.getStringArrayExtra("mainAddress")
+                val detailAddress = data?.getStringArrayExtra("detailAddress")
+
+                addressText.text = "${mainAddress} / ${detailAddress}"
+            }
         }
     }
 }
