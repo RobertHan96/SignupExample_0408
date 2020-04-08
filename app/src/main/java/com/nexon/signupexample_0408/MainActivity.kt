c8fd21a561a6b9fb602ca07d8e456decfc3aa84d
RobertHan96 <yshan4329@gmail.com>
2020-04-08 21:10:11 +0900

    7th commit : 화면 전환 기능 추가

--- app/src/main/java/com/nexon/signupexample_0408/MainActivity.kt
+++ app/src/main/java/com/nexon/signupexample_0408/MainActivity.kt
@@ -1,12 +1,21 @@
 package com.nexon.signupexample_0408
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        nextBtn.setOnClickListener {
+//            Intent함수로 하나의 화면을 객체하함, 출발점(this/self), 도착점
+            val myIntent = Intent(this, MyPageActivity::class.java)
+            startActivity(myIntent)
+        }
+
     }
 }
